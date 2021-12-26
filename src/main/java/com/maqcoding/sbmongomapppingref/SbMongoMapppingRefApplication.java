dde3fe705ab7a24f9073216d140ab0878b5149d4
Maqsood <maqsoodansari818@gmail.com>
2021-12-26 16:26:27 +0530

    spring boot mongo mapping reference initial commit

---
+++ src/main/java/com/maqcoding/sbmongomapppingref/SbMongoMapppingRefApplication.java
@@ -0,0 +1,13 @@
+package com.maqcoding.sbmongomapppingref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongoMapppingRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMongoMapppingRefApplication.class, args);
+	}
+
+}
